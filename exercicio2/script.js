3da459d8dec994ba605613a01ef010565a931486
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-09-26 21:15:09 -0300

    Sanderson-BarbosaC

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let imput = document.getElementById(`nome`)
+console.log(imput.value);
+imput.value = ""
+
+let imput2 = document.getElementById(`endereco`)
+console.log(imput2.value);
+imput2.value = ""
+
+let imput3 = document.getElementById(`email`)
+console.log(imput3.value);
+imput3.value = ""
+
---
+++ fixacao/script.js
@@ -0,0 +1,8 @@
+let nome = document.getElementById(`paragrafo`)
+
+let texto = document.getElementById(`texto`)
+
+function troca() {
+    nome.innerHTML = texto.value
+}
+    
